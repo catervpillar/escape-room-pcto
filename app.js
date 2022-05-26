97f50c7a34fe1b535ef2270abbbdb9686377e963
AlessioG98 <alessio.galassi@studenti.unicam.it>
2022-05-26 15:54:57 +0200

    split content html

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-// const markers = document.querySelectorAll('.marker');
+window.addEventListener('load', (event) => {
 
-const marker1 = document.getElementById('marker1');
-const marker2 = document.getElementById('marker1');
-const marker3 = document.getElementById('marker1');
-const marker4 = document.getElementById('marker1');
+    function markerFunction(event) {
+        console.log(event.target.id);
+        console.log(event.target);
+        let marker = document.getElementById(event.target.id)
+        marker.classList.add('visited-marker');
+    }
 
-marker1.addEventListener('click', marker1Function);
+    let markers = document.querySelectorAll('.marker');
+    markers.forEach(function (marker) {
+        marker.addEventListener('click', markerFunction);
+    });
 
-function marker1Function(event) {
-    console.log(event.target.id);
-    marker1.classList.add('visited-marker');
-}
+});
